a1c1bab46394a11491b9e7ea53e3d42843819879
HyCraftHD <hycrafthd@live.de>
2020-09-21 16:50:45 +0200

    Add basic command for toggle tie bedrock breaking value

--- src/main/java/info/u_team/useful_dragon_eggs/command/DragonEggsCommand.java
+++ src/main/java/info/u_team/useful_dragon_eggs/command/DragonEggsCommand.java
@@ -1,14 +1,23 @@
 package info.u_team.useful_dragon_eggs.command;
 
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.arguments.BoolArgumentType;
 
-import net.minecraft.command.CommandSource;
+import net.minecraft.command.*;
 
 public class DragonEggsCommand {
 	
 	public static void register(CommandDispatcher<CommandSource> dispatcher) {
-		dispatcher.register(LiteralArgumentBuilder.literal("dragoneggs"));
+		dispatcher.register(Commands.literal("dragoneggs") //
+				.then(Commands.literal("bedrock-breaking") //
+						.requires(source -> source.hasPermissionLevel(2)) //
+						.then(Commands.argument("value", BoolArgumentType.bool())).executes(context -> {
+							return bedrockBreaking(context.getSource(), BoolArgumentType.getBool(context, "value"));
+						})));
+	}
+	
+	private static int bedrockBreaking(CommandSource source, boolean value) {
+		return 0;
 	}
 	
 }
