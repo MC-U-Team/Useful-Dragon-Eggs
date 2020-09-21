ff1a1ebe8a31446c0a4b739584892bc35ea31e34
HyCraftHD <hycrafthd@live.de>
2020-09-21 14:40:34 +0200

    Add base classes for the mod

---
+++ src/main/java/info/u_team/useful_dragon_eggs/UsefulDragonEggsMod.java
@@ -0,0 +1,17 @@
+package info.u_team.useful_dragon_eggs;
+
+import info.u_team.u_team_core.util.annotation.AnnotationManager;
+import info.u_team.u_team_core.util.verify.JarSignVerifier;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(UsefulDragonEggsMod.MODID)
+public class UsefulDragonEggsMod {
+	
+	public static final String MODID = "usefuldragoneggs";
+	
+	public UsefulDragonEggsMod() {
+		JarSignVerifier.checkSigned(MODID);
+		
+		AnnotationManager.callAnnotations(MODID);
+	}
+}
---
+++ src/main/java/info/u_team/useful_dragon_eggs/init/UsefulDragonEggsCommonConstruct.java
@@ -0,0 +1,12 @@
+package info.u_team.useful_dragon_eggs.init;
+
+import info.u_team.u_team_core.api.construct.*;
+import info.u_team.useful_dragon_eggs.UsefulDragonEggsMod;
+
+@Construct(modid = UsefulDragonEggsMod.MODID)
+public class UsefulDragonEggsCommonConstruct implements IModConstruct {
+	
+	@Override
+	public void construct() {
+	}
+}
