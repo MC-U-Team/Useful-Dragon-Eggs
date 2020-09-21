c5048207b6efae59cdd10c8e9a49b25bd1b8140c
HyCraftHD <hycrafthd@live.de>
2020-09-21 15:33:51 +0200

    Register the event handler

--- src/main/java/info/u_team/useful_dragon_eggs/init/UsefulDragonEggsCommonConstruct.java
+++ src/main/java/info/u_team/useful_dragon_eggs/init/UsefulDragonEggsCommonConstruct.java
@@ -1,12 +1,15 @@
 package info.u_team.useful_dragon_eggs.init;
 
 import info.u_team.u_team_core.api.construct.*;
+import info.u_team.u_team_core.util.registry.BusRegister;
 import info.u_team.useful_dragon_eggs.UsefulDragonEggsMod;
+import info.u_team.useful_dragon_eggs.event.UsefulDragonEggsFallHandler;
 
 @Construct(modid = UsefulDragonEggsMod.MODID)
 public class UsefulDragonEggsCommonConstruct implements IModConstruct {
 	
 	@Override
 	public void construct() {
+		BusRegister.registerForge(UsefulDragonEggsFallHandler::registerForge);
 	}
 }
