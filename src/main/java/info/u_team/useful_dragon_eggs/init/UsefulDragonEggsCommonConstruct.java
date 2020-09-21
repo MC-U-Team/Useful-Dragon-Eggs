3d1fa03ad971e23e38adef2b67f1ed678fdb1334
HyCraftHD <hycrafthd@live.de>
2020-09-21 16:57:39 +0200

    Set this mod to be ignored on the client / server

--- src/main/java/info/u_team/useful_dragon_eggs/init/UsefulDragonEggsCommonConstruct.java
+++ src/main/java/info/u_team/useful_dragon_eggs/init/UsefulDragonEggsCommonConstruct.java
@@ -1,15 +1,21 @@
 package info.u_team.useful_dragon_eggs.init;
 
+import org.apache.commons.lang3.tuple.Pair;
+
 import info.u_team.u_team_core.api.construct.*;
 import info.u_team.u_team_core.util.registry.BusRegister;
 import info.u_team.useful_dragon_eggs.UsefulDragonEggsMod;
 import info.u_team.useful_dragon_eggs.event.UsefulDragonEggsFallHandler;
+import net.minecraftforge.fml.*;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 @Construct(modid = UsefulDragonEggsMod.MODID)
 public class UsefulDragonEggsCommonConstruct implements IModConstruct {
 	
 	@Override
 	public void construct() {
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+		
 		BusRegister.registerForge(UsefulDragonEggsCommands::registerForge);
 		BusRegister.registerForge(UsefulDragonEggsFallHandler::registerForge);
 	}
