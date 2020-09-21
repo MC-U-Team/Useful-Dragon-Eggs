0c00beb2ab945d2579af1407f4c27c7acf924f0c
HyCraftHD <hycrafthd@live.de>
2020-09-21 17:03:38 +0200

    Set the value of the config in the command

--- src/main/java/info/u_team/useful_dragon_eggs/command/DragonEggsCommand.java
+++ src/main/java/info/u_team/useful_dragon_eggs/command/DragonEggsCommand.java
@@ -3,7 +3,9 @@ package info.u_team.useful_dragon_eggs.command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.BoolArgumentType;
 
+import info.u_team.useful_dragon_eggs.config.ServerConfig;
 import net.minecraft.command.*;
+import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
 
 public class DragonEggsCommand {
 	
@@ -12,11 +14,14 @@ public class DragonEggsCommand {
 				.then(Commands.literal("bedrock-breaking") //
 						.requires(source -> source.hasPermissionLevel(2)) //
 						.then(Commands.argument("value", BoolArgumentType.bool())).executes(context -> {
-							return bedrockBreaking(context.getSource(), BoolArgumentType.getBool(context, "value"));
+							return bedrockBreakingSet(context.getSource(), BoolArgumentType.getBool(context, "value"));
 						})));
 	}
 	
-	private static int bedrockBreaking(CommandSource source, boolean value) {
+	private static int bedrockBreakingSet(CommandSource source, boolean value) {
+		final BooleanValue bedrockBreaking = ServerConfig.getInstance().bedrockBreaking;
+		bedrockBreaking.set(value);
+		bedrockBreaking.save();
 		return 0;
 	}
 	
